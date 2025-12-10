cdbd5ebb4d13ae2c321af482aeac941f23f6a91d
Your Name <your-email@example.com>
2025-12-10 16:33:50 +0530

    first

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
+
 import "./Home.css";
 
 import gemPhone from "../assets/ganesh.png";
